0cfe2642afe8e7b3f56a01bd9a3dfcfe58481b8a
msaito-ia <saito.masahiro@inter-action.co.jp>
2022-09-28 17:41:07 +0900

    divide exe and dll

---
+++ src/my_math_dll/my_math.h
@@ -0,0 +1,12 @@
+// Copyright 2022 saito
+#ifndef SRC_MY_MATH_DLL_MY_MATH_H_
+#define SRC_MY_MATH_DLL_MY_MATH_H_
+
+#include <iostream>
+
+
+
+double sqr(double);
+double sum(double, double);
+
+#endif  // SRC_MY_MATH_DLL_MY_MATH_H_
