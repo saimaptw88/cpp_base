4bc667187aa4c78fe276c7610ff091ad0a6aaad2
msaito-ia <saito.masahiro@inter-action.co.jp>
2022-09-28 13:24:30 +0900

    remove unneseccary files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cpp-dev
-# Build directory: /cpp-dev/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "/cpp-dev/build/test/Test")
-subdirs("src")
-subdirs("test")
