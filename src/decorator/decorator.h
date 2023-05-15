19ed0e6794a5a479400bb9f2313b564a476cd3b7
msaito-ia <saito.masahiro@inter-action.co.jp>
2023-05-15 10:29:58 +0900

    add files

---
+++ src/decorator/decorator.h
@@ -0,0 +1,11 @@
+// Copyright 2023 saito
+#ifndef SRC_DECORATOR_DECORATOR_H_
+#define SRC_DECORATOR_DECORATOR_H_
+
+#include <iostream>
+
+
+int sum(int, int);
+
+#endif  // SRC_DECORATOR_DECORATOR_H_
+
